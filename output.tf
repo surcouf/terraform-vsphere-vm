119cfb74361afbe7929ddffaca2f98db1e4f2ad3
Raphaël Bordet <raphael.bordet.ext@maia-space.com>
2024-10-04 17:17:21 +0200

    Revert "Define playbook output."
    This reverts commit 342ad7d2e31472473a49c44e9e379b7214b6a557.

--- output.tf
+++ output.tf
@@ -32,8 +32,3 @@ output "disk" {
   description = "Disks of the deployed VM"
   value       = vsphere_virtual_machine.vm.*.disk
 }
-
-output "playbook" {
-  description = "Output of Ansible playbook execution"
-  value = ansible_playbook.playbook.*.ansible_playbook_stdout
-}
