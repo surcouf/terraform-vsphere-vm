b9ea03e439a2cc1765ec25988f75bd100dbe975c
Raphaël Bordet <raphael.bordet.ext@maia-space.com>
2024-10-11 10:31:39 +0200

    ansible_variables: set type to string

--- variables.tf
+++ variables.tf
@@ -612,8 +612,8 @@ variable "ansible_tags" {
 
 variable "ansible_variables" {
   description = "Ansible variables"
-  type        = map(string)
-  default     = {}
+  type        = string
+  default     = ""
 }
 
 variable "http_proxy" {
